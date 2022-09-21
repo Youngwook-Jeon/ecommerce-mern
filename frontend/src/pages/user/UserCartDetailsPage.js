41a9b5bc114c950811c3c690e813981b9f7ee26e
Youngwook-Jeon <ela87@naver.com>
2022-09-21 19:37:42 +0900

    Finish template for user orders page

--- frontend/src/pages/user/UserCartDetailsPage.js
+++ frontend/src/pages/user/UserCartDetailsPage.js
@@ -1,5 +1,89 @@
-const UserCartDetailsPage = () => {
-    return <p>This is a user cart details page</p>
-}
-
-export default UserCartDetailsPage;
+import {
+    Alert,
+    Button,
+    Col,
+    Container,
+    Form,
+    ListGroup,
+    Row,
+  } from "react-bootstrap";
+  import CartItemComponent from "../../components/CartItemComponent";
+  
+  const UserCartDetailsPage = () => {
+    return (
+      <Container fluid>
+        <Row className="mt-4">
+          <h1>Cart Details</h1>
+          <Col md={8}>
+            <br />
+            <Row>
+              <Col md={6}>
+                <h2>Shipping</h2>
+                <b>Name</b>: John Doe <br />
+                <b>Address</b>: 8739 Mayflower St. Los Angeles, CA 90063 <br />
+                <b>Phone</b>: 888 777 666
+              </Col>
+              <Col md={6}>
+                <h2>Payment Method</h2>
+                <Form.Select>
+                  <option value="pp">PayPal</option>
+                  <option value="cod">
+                    Cash On Delivery (delivery may be delayed)
+                  </option>
+                </Form.Select>
+              </Col>
+              <Row>
+                <Col>
+                  <Alert className="mt-3" variant="danger">
+                    Not delivered. In order to make order, fill out your profile with correct address, city etc.
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert className="mt-3" variant="success">
+                    Not paid yet
+                  </Alert>
+                </Col>
+              </Row>
+            </Row>
+            <br />
+  
+            <h2>Order Items</h2>
+            <ListGroup variant="flush">
+              {Array.from({ length: 3 }).map((item, idx) => (
+                <CartItemComponent key={idx} />
+              ))}
+            </ListGroup>
+          </Col>
+          <Col md={4}>
+            <ListGroup>
+              <ListGroup.Item>
+                <h3>Order Summary</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Items Price (after tax): <span className="fw-bold">$892</span>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Shipping: <span className="fw-bold">included</span>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Tax: <span className="fw-bold">included</span>
+              </ListGroup.Item>
+              <ListGroup.Item className="text-danger">
+                Total Price: <span className="fw-bold">$904</span>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <div className="d-grid gap-2">
+                  <Button size="lg" variant="danger" type="button">
+                    Pay for the Order
+                  </Button>
+                </div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+  
+  export default UserCartDetailsPage;
+  
