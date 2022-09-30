2e59db1e4b3c1670294e0e77219ed66ce73b6719
Youngwook-Jeon <ela87@naver.com>
2022-09-30 22:36:10 +0900

    Filter results of queries for products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,13 +3,42 @@ const Product = require("../models/ProductModel");
 
 const getProducts = async (req, res, next) => {
   try {
+    let query = {};
+    let queryCondition = false;
+    let priceQueryCondition = {};
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
+
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
     const pageNum = Number(req.query.pageNum) || 1;
-    const totalProducts = await Product.countDocuments({});
-    const products = await Product.find({})
+
+    // sort by name, price etc
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if (sortOption) {
+      let sortOpt = sortOption.split("_");
+      sort = { [sortOpt[0]]: Number(sortOpt[1]) };
+    }
+
+    const totalProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
       .skip(RECORDS_PER_PAGE * (pageNum - 1))
-      .sort({ name: 1 })
+      .sort(sort)
       .limit(RECORDS_PER_PAGE);
-      
+
     res.json({
       products,
       pageNum,
