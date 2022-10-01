dcbd91afbfed0c426a7dd94c45538b2dda264413
Youngwook-Jeon <ela87@naver.com>
2022-10-02 03:24:34 +0900

    Get products by attrs / Get products by search queries

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,14 +36,22 @@ const getProducts = async (req, res, next) => {
       };
     }
 
-    if (queryCondition) {
-      query = {
-        $and: [
-          priceQueryCondition,
-          ratingQueryCondition,
-          categoryQueryCondition,
-        ],
-      };
+    let attrsQueryCondition = [];
+    if (req.query.attrs) {
+      // attrs = 'RAM-1TB-2TB-4TB,color-blue-red'
+      attrsQueryCondition = req.query.attrs.split(",").reduce((acc, item) => {
+        if (item) {
+          let a = item.split("-");
+          let values = [...a];
+          values.shift();
+          let a1 = {
+            attrs: { $elemMatch: { key: a[0], value: { $in: values } } },
+          };
+          acc.push(a1);
+          return acc;
+        } else return acc;
+      }, []);
+      queryCondition = true;
     }
 
     const pageNum = Number(req.query.pageNum) || 1;
@@ -56,8 +64,33 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) };
     }
 
+    const searchQuery = req.params.searchQuery || "";
+    let searchQueryCondition = {};
+    let select = {};
+    if (searchQuery) {
+      queryCondition = true;
+      searchQueryCondition = { $text: { $search: searchQuery } };
+      select = {
+        score: { $meta: "textScore" }
+      }
+      sort = { score: { $meta: "textScore" }}
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [
+          priceQueryCondition,
+          ratingQueryCondition,
+          categoryQueryCondition,
+          searchQueryCondition,
+          ...attrsQueryCondition,
+        ],
+      };
+    }
+
     const totalProducts = await Product.countDocuments(query);
     const products = await Product.find(query)
+      .select(select)
       .skip(RECORDS_PER_PAGE * (pageNum - 1))
       .sort(sort)
       .limit(RECORDS_PER_PAGE);
