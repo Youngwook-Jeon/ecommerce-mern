3822522a284d62880bcce40418e190a954d24753
Youngwook-Jeon <ela87@naver.com>
2022-10-09 21:29:05 +0900

    Develop login funtionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/UserModel");
 const generateAuthToken = require("../utils/generateAuthToken");
-const { hashPassword } = require("../utils/hashPassword");
+const { hashPassword, comparePasswords } = require("../utils/hashPassword");
 
 const getUsers = async (req, res, next) => {
   try {
@@ -61,4 +61,54 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-module.exports = { getUsers, registerUser };
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password, doNotLogout } = req.body;
+    if (!(email && password)) {
+      return res.status(400).send("All inputs are required");
+    }
+
+    const user = await User.findOne({ email });
+    if (user && comparePasswords(password, user.password)) {
+      let cookieParams = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      };
+
+      if (doNotLogout) {
+        cookieParams = { ...cookieParams, maxAge: 1000 * 60 * 60 * 24 * 14 }; // two weeks
+      }
+
+      return res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.lastName,
+            user.email,
+            user.isAdmin
+          ),
+          cookieParams
+        )
+        .json({
+          success: "user logged in successfully",
+          userLoggedIn: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            doNotLogout,
+          },
+        });
+    } else {
+      return res.status(401).send("wrong credentials");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getUsers, registerUser, loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getUsers, registerUser } = require("../controllers/userController");
+const { getUsers, registerUser, loginUser } = require("../controllers/userController");
 
 router.get("/", getUsers);
 
 // admin routes:
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
