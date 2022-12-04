3a36f40d58bac7a52c5551c682ee859da7df3e17
Youngwook-Jeon <ela87@naver.com>
2022-12-04 16:01:07 +0900

    Add API call to get all products

--- frontend/src/components/ProductForListComponent.js
+++ frontend/src/components/ProductForListComponent.js
@@ -2,29 +2,25 @@ import { Card, Button, Row, Col } from "react-bootstrap";
 import { Rating } from "react-simple-star-rating";
 import { LinkContainer } from "react-router-bootstrap";
 
-const ProductForListComponent = ({ images, idx }) => {
+const ProductForListComponent = ({ productId, name, description, price, images, rating, reviewsNumber }) => {
   return (
     <Card style={{ marginTop: "30px", marginBottom: "50px" }}>
       <Row>
         <Col lg={5}>
-          <Card.Img crossOrigin="anonymous" variant="top" src={"/images/" + images[idx] + "-category.png"} />
+          <Card.Img crossOrigin="anonymous" variant="top" src={images[0] ? images[0].path : ""} />
         </Col>
         <Col lg={7}>
           <Card.Body>
-            <Card.Title>The standard chunk of Lorem Ipsum</Card.Title>
+            <Card.Title>{name}</Card.Title>
             <Card.Text>
-              The standard chunk of Lorem Ipsum used since the 1500s is
-              reproduced below for those interested. Sections 1.10.32 and
-              1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
-              reproduced in their exact original form, accompanied by English
-              versions from the 1914 translation by H. Rackham.
+              {description}
             </Card.Text>
             <Card.Text>
-              <Rating readonly size={20} initialValue={5} /> (1)
+              <Rating readonly size={20} initialValue={rating} /> ({reviewsNumber})
             </Card.Text>
             <Card.Text className="h4">
-              $124{" "}
-              <LinkContainer to="/product-details">
+              ${price}{" "}
+              <LinkContainer to={`/product-details/${productId}`}>
                 <Button variant="danger">See product</Button>
               </LinkContainer>
             </Card.Text>
