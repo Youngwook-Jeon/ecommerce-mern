224b3b900abeac26bce0b229c86b2352144cdc23
Youngwook-Jeon <ela87@naver.com>
2022-12-25 22:17:40 +0900

    Fetch and display details of a product for a user

--- frontend/src/pages/ProductDetailsPage.js
+++ frontend/src/pages/ProductDetailsPage.js
@@ -1,12 +1,22 @@
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/actions/cartActions";
 import ProductDetailsPageComponent from "./components/ProductDetailsPageComponent";
+import axios from "axios";
+
+const getProductDetails = async (id) => {
+  const { data } = await axios.get(`/api/products/get-one/${id}`);
+  return data;
+};
 
 const ProductDetailsPage = () => {
   const dispatch = useDispatch();
 
   return (
-    <ProductDetailsPageComponent addToCartReduxAction={addToCart} reduxDispatch={dispatch} />
+    <ProductDetailsPageComponent
+      addToCartReduxAction={addToCart}
+      reduxDispatch={dispatch}
+      getProductDetails={getProductDetails}
+    />
   );
 };
 
