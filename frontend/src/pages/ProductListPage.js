0bfe4e92bfd1d489c529984c7774966382f800f7
Youngwook-Jeon <ela87@naver.com>
2023-01-05 14:52:05 +0900

    Filter products

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -5,19 +5,63 @@ import { useSelector } from "react-redux";
 // ex: "&price=50&rating=1,2,3&category=a,b,c,d&attrs=color-red-blue,size-1TB-2TB"
 let filtersUrl = "";
 
-const getProducts = async (categoryName = "", pageNum = null, searchQuery = "", filters = {}, sortOption = "") => {
+const proceedFilters = (filters) => {
+  filtersUrl = "";
+  Object.keys(filters).map((key, index) => {
+    if (key === "price") filtersUrl += `&price=${filters[key]}`;
+    else if (key === "rating") {
+      let rat = "";
+      Object.keys(filters[key]).map((key2, index2) => {
+        if (filters[key][key2]) rat += `${key2},`;
+        return "";
+      });
+      filtersUrl += "&rating=" + rat;
+    } else if (key === "category") {
+      let cat = "";
+      Object.keys(filters[key]).map((key3, index3) => {
+        if (filters[key][key3]) cat += `${key3},`;
+        return "";
+      });
+      filtersUrl += "&category=" + cat;
+    } else if (key === "attrs") {
+      if (filters[key].length > 0) {
+        let val = filters[key].reduce((acc, item) => {
+          let key = item.key;
+          let vals = item.values.join("-");
+          return acc + key + "-" + vals + ",";
+        }, "");
+        filtersUrl += "&attrs=" + val;
+      }
+    }
+    return "";
+  });
+
+  return filtersUrl;
+};
+
+const getProducts = async (
+  categoryName = "",
+  pageNum = null,
+  searchQuery = "",
+  filters = {},
+  sortOption = ""
+) => {
+  filtersUrl = proceedFilters(filters);
   const search = searchQuery ? `search/${searchQuery}/` : "";
   const category = categoryName ? `category/${categoryName}/` : "";
   const url = `/api/products/${category}${search}?pageNum=${pageNum}${filtersUrl}&sort=${sortOption}`;
   const { data } = await axios.get(url);
   return data;
-}
+};
 
 const ProductListPage = () => {
   const { categories } = useSelector((state) => state.getCategories);
 
   return (
-    <ProductListPageComponent getProducts={getProducts} categories={categories} />
+    <ProductListPageComponent
+      getProducts={getProducts}
+      categories={categories}
+    />
   );
 };
 
