9b74defa00fb60f293baa6922b0a06bd5e229aa3
Youngwook-Jeon <ela87@naver.com>
2023-01-09 23:34:51 +0900

    Make api request to fetch bestsellers from our home page

--- frontend/src/pages/components/HomePageComponent.js
+++ frontend/src/pages/components/HomePageComponent.js
@@ -3,18 +3,31 @@ import ProductCarouselComponent from "../../components/ProductCarouselComponent"
 import { Row, Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
-const HomePageComponent = ({ categories }) => {
+const HomePageComponent = ({ categories, getBestSellers }) => {
   const [mainCategories, setMainCategories] = useState([]);
+  const [bestSellers, setBestSellers] = useState([]);
 
   useEffect(() => {
+    getBestSellers()
+      .then((data) => {
+        setBestSellers(data);
+      })
+      .catch((err) =>
+        console.log(
+          err.response.data.message
+            ? err.response.data.message
+            : err.response.data
+        )
+      );
+
     setMainCategories((cat) =>
       categories.filter((item) => !item.name.includes("/"))
     );
-  }, [categories]);
+  }, [categories, getBestSellers]);
 
   return (
     <>
-      <ProductCarouselComponent />
+      <ProductCarouselComponent bestSellers={bestSellers} />
       <Container>
         <Row xs={1} md={2} className="g-4 mt-5">
           {mainCategories.map((category, idx) => (
