dc8d41fe17f01282558f0071a81eb007d3745e77
Youngwook-Jeon <ela87@naver.com>
2023-01-14 22:32:05 +0900

    Send socket messages from client

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -1,7 +1,45 @@
 import "../../chats.css";
+import { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+import { useSelector } from "react-redux";
 
 const UserChatComponent = () => {
-  return (
+  const [socket, setSocket] = useState(false);
+  const [chat, setChat] = useState([]);
+
+  const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
+
+  useEffect(() => {
+    if (!userInfo.isAdmin) {
+      const socket = socketIOClient();
+      setSocket(socket);
+      return () => socket.disconnect();
+    }
+  }, [userInfo.isAdmin]);
+
+  const clientSubmitChatMsg = (e) => {
+    if (e.keyCode && e.keyCode !== 13) {
+      return;
+    }
+
+    const msg = document.getElementById("clientChatMsg");
+    let v = msg.value.trim();
+    if (v === "" || v === null || v === false || !v) {
+      return;
+    }
+    socket.emit("client sends message", v);
+    setChat((chat) => {
+      return [...chat, { client: v }];
+    });
+    msg.focus();
+    setTimeout(() => {
+      msg.value = "";
+      const chatMessages = document.querySelector(".chat-msg");
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }, 200);
+  };
+
+  return !userInfo.isAdmin ? (
     <>
       <input type="checkbox" id="check" />
       <label className="chat-btn" htmlFor="check">
@@ -15,28 +53,38 @@ const UserChatComponent = () => {
         </div>
         <div className="chat-form">
           <div className="chat-msg">
-            {Array.from({ length: 20 }).map((_, id) => (
+            {chat.map((item, id) => (
               <div key={id}>
-                <p>
-                  <b>You wrote:</b> Hello, world! This is a toast message.
-                </p>
-                <p className="bg-primary p-3 ms-4 text-light rounded-pill">
-                  <b>Support wrote:</b> Hello, world! This is a toast message.
-                </p>
+                {item.client && (
+                  <p>
+                    <b>You wrote:</b> {item.client}
+                  </p>
+                )}
+                {item.admin && (
+                  <p className="bg-primary p-3 ms-4 text-light rounded-pill">
+                    <b>Support wrote:</b> {item.admin}
+                  </p>
+                )}
               </div>
             ))}
           </div>
           <textarea
+            onKeyUp={(e) => clientSubmitChatMsg(e)}
             id="clientChatMsg"
             className="form-control"
             placeholder="Your Text Message"
           ></textarea>
 
-          <button className="btn btn-success btn-block">Submit</button>
+          <button
+            onClick={(e) => clientSubmitChatMsg(e)}
+            className="btn btn-success btn-block"
+          >
+            Submit
+          </button>
         </div>
       </div>
     </>
-  );
+  ) : null;
 };
 
 export default UserChatComponent;
